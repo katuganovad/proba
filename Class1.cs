3ff496446124dd1f594521a6dc0814009165f2c5
katuganovad <107763861+katuganovad@users.noreply.github.com>
2023-09-07 14:46:10 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба4._1
+{
+   
+
+        public class Fridge
+        {
+            string name;//производитель
+            double currentT;//текущая температура
+            double setT;//заданная температура
+            double deviationT;//допустимое отклонение от заданной температуры
+            double freezespeed;//скорость охлаждения
+            double equalspeed;//скорость выравнивания температуры с окружающей средой
+            double sredaT;//температура окружающей среды
+            int hol;
+            public Fridge(string name, double currentT, double setT, double deviationT, double freezespeed, double equalspeed, double sredaT)
+            {
+                hol++;
+                this.name = name;
+                this.setT = setT;
+                Temper = currentT;
+                this.deviationT = deviationT;
+                SpeedF = freezespeed;
+                SpeedS = equalspeed;
+                this.sredaT = sredaT;
+            }
+
+            public string passport()
+            {
+                return string.Format($"Название холодильника: {name}\n" +
+    
+                    $"Текущая температура в камере холодильника: {currentT}\n" +
+    
+                    $"Заданная температура в камере холодильника: {setT}\n" +
+    
+                    $"Разрешенное отклонение от заданной температуры: {deviationT}\n" +
+    
+                    $"Скорость охлаждения: {freezespeed}\n" +
+    
+                    $"Скорость выравнивания температуры с окружающей средой: {equalspeed}\n" +
+    
+                    $"Температура окружающей среды: {sredaT}\n");
+            }
+            public void currentTempF()
+            {
+                Console.WriteLine("Текущая температура в холодильнике: " + currentT);
+            }
+            public void freezinTime(double time)//time-время, в течение которого необходимо охлаждать
+            {
+                double holodT = currentT - (freezespeed * time);
+                if (holodT > (setT - deviationT))
+                {
+                    Temper = holodT;
+                    Console.WriteLine("За время " + time + "произошло охлаждение до  " + currentT);
+                }
+                else
+                {
+                    Temper = setT;
+                    Console.WriteLine("За время " + time + "произошло охлаждение до  " + currentT);
+                }
+            }
+            public void waitingtime(double time)
+            {
+                double nagrevT = currentT + (equalspeed * time);
+                if (nagrevT < sredaT)
+                {
+                    Temper = nagrevT;
+                    Console.WriteLine("За время " + time + "произошло  нагревания до  " + currentT);
+                }
+                else
+                {
+                    Temper = sredaT;
+                    Console.WriteLine("За время " + time + "произошло  нагревания до  " + currentT);
+                }
+            }
+            public double Temper
+            {   get { return currentT; }
+                
+                set { if ((value > sredaT) || (value < setT))
+                {
+                    Console.WriteLine("Вы ввелии невозможную температуру,  устновлено значение по умолчанию равное -2!");
+                    currentT = -2;
+                }
+                else
+                {
+                    currentT = value;
+                }
+                }
+            }
+        public double SpeedF
+        {
+            get { return freezespeed; }
+
+            set
+            {
+                if (value <= 0) 
+                {
+                    Console.WriteLine("Вы ввелии невозможную скорость охлаждения, устновлено значение по умолчанию равное 0.5!");
+                    freezespeed=0.5;
+                }
+                else
+                {
+                    freezespeed = value;
+                }
+            }
+        }
+        public double SpeedS
+        {
+            get { return equalspeed; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Вы ввелии невозможную скорость выравнивания, устновлено значение по умолчанию равное 2!");
+                    equalspeed = 2;
+                }
+                else
+                {
+                    equalspeed = value;
+                }
+            }
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Холодильник";
+            Console.WriteLine("Введите производителя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите текущую температуру");
+            double currentT = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите заданную температуру");
+            double setT = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите допустимое отклонение от заданной температуры");
+            double deviationT = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите скорость охлаждения");
+            double freezespeed = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите скорость выравнивания температуры с окружающей средой");
+            double equalspeed = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите температурe окружающей средой");
+            double sredaT = Convert.ToDouble(Console.ReadLine());
+            Fridge holod = new Fridge(name, currentT, setT, deviationT, freezespeed, equalspeed, sredaT);
+            for (; ; )
+            {
+                Console.WriteLine("1 - Узнать текущую температуру в камере");
+                Console.WriteLine("2 - Охлаждать в течении какого-то времени");
+                Console.WriteLine("3 - Ожидать в течении какого-то времени");
+                Console.WriteLine("4 - Изменить температуру холодильника с помощью свойств");
+                Console.WriteLine("5 - Закончить работу");
+                Console.WriteLine("Введите действие, которое необходимо выполнить: ");
+                int dev = int.Parse(Console.ReadLine());
+                switch (dev)
+                {
+                    case 1:
+                        holod.currentTempF();
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите время охлаждения");
+                        double time = Convert.ToDouble(Console.ReadLine());
+                        holod.freezinTime(time);
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите время ожидания");
+                        double time1 = Convert.ToDouble(Console.ReadLine());
+                        holod.waitingtime(time1);
+                        break;
+                    case 4:
+                        Console.WriteLine("Текущая температура холодильника {0} \n", currentT);
+                        Console.WriteLine("устанавливаем новую температуру Т = {0} \n", -1); 
+                        holod.Temper = -1;
+                        Console.WriteLine("Изменённая температура холодильника {0} \n", holod.Temper);
+                        Console.Read();
+
+                        break;
+                    case 5: 
+                        holod.passport(); 
+                        break;
+                }
+            }
+        }
+    }
+}
